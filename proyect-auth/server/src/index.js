0d3ee04bb583507e354dd4af3356c9d5416e26fc
Patricio <patricio.zetazeta@gmail.com>
2025-12-05 10:41:09 -0300

    cambios para el deploy

--- proyect-auth/server/src/index.js
+++ proyect-auth/server/src/index.js
@@ -13,4 +13,5 @@ app.use("/horario", horarioRoutes);
 app.use("/notas", notasRoutes);
 app.use("/evento", eventosRoutes);
 
-app.listen(3000, () => console.log("API corriendo en puerto 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API corriendo en puerto ${PORT}`));
